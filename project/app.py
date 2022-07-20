d5a3262965e2a93b3a5c80f20f532cc79767e3ae
evgenyzhupanik <zhenya1975@mail.ru>
2022-07-20 22:51:00 +0300

    init

---
+++ project/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, flash
+from extensions import extensions
+from routes.routes import home
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/fms_v3_db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@db:5432/fms_v3_db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'secret string'
+
+
+db = extensions.db
+
+migrate = extensions.migrate
+socketio = extensions.socketio
+
+socketio.init_app(app)
+
+db.init_app(app)
+migrate.init_app(app, db, render_as_batch=True)
+
+app.register_blueprint(home)
+
+if __name__ == '__main__':
+    app.run(debug=True)
