de090dbdaf1f298c416b84e9454aa2f1ae84d6fa
evgenyzhupanik <zhenya1975@mail.ru>
2022-07-24 19:17:49 +0300

    chmode

--- project/delete_alembic.py
+++ project/delete_alembic.py
@@ -1,2 +1,23 @@
-from app import app
+from psycopg2 import connect
+import psycopg2
 
+config = {
+    # "host": "localhost",
+    "dbname": "fms_v3_db",
+    "user": "postgres",
+    "password": "123456",
+    "port": "5432"
+}
+
+
+def query():
+
+    sql_query = 'DROP TABLE IF EXISTS alembic_version;'
+    with connect(**config) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql_query)
+            # res = cur.fetchall()
+            # print(res)
+
+
+query()
